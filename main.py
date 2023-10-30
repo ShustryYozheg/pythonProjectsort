b35a852e9c122142706af86f7f1a0724d9ad320d
ShustryYozheg <s.takzhanov@gmail.com>
2023-10-30 14:23:37 +0300

    версия 1

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import face_recognition
 import shutil
 
 # Путь к папке с вашими фотографиями (позитивными образцами)
-positive_samples_folder = 'путь_к_папке_с_вашими_фото'
+positive_samples_folder = 'D:\\for py\\positive'
 
 # Папка, в которой будут сохранены фотографии с вашим лицом
-output_folder = 'путь_к_папке_для_сохранения_фотографий_с_вашим_лицом'
+output_folder = 'D:\\for py\\positive\\done'
 
 # Загрузим все фотографии из папки и получим кодировки лиц для каждой из них
 positive_face_encodings = []
@@ -23,9 +23,9 @@ for filename in os.listdir(positive_samples_folder):
             positive_face_encodings.append(face_encoding)
 
 # Пройдем по всем фотографиям в папке, в которой вы хотите найти фотографии с вашим лицом
-for filename in os.listdir(input_folder):
+for filename in os.listdir('D:\\for py\\Photos1'):
     if filename.endswith('.jpg') or filename.endswith('.png'):
-        image_path = os.path.join(input_folder, filename)
+        image_path = os.path.join('D:\\for py\\Photos1', filename)
         image = face_recognition.load_image_file(image_path)
         face_locations = face_recognition.face_locations(image)
         face_encodings = face_recognition.face_encodings(image, face_locations)
@@ -37,5 +37,5 @@ for filename in os.listdir(input_folder):
             if any(matches):
                 print(f"Найдено ваше лицо на фотографии: {filename}")
                 # Переместим или скопируем фотографию с вашим лицом в выходную папку
-                shutil.copy(image_path, os.path.join(output_folder, filename))
+                shutil.copy(image_path, os.path.join('D:\\for py', filename))
                 break
